f38c55c76c2ad6065ac2c612cf9dfdda3835af3b
Qbisst <ismaelaperez@outlook.com>
2019-06-21 22:23:30 -0400

    created App component

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import Intro from './components/Intro';
+import Intro from '../Intro';
 import './App.css';
 
 
 class App extends Component {
+  state = {
+    series: []
+  }
   render() {
     return (
       <div className="App">
@@ -11,6 +14,7 @@ class App extends Component {
           <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved TV series"/>
+        the length of series array is: {this.state.series.length}
       </div>
     );
   }
