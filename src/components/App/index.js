4c22b6888ec89514f7f38b00afa790f3484b8eec
Qbisst <ismaelaperez@outlook.com>
2019-06-21 22:34:01 -0400

    created a componentDidMount

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,13 +7,20 @@ class App extends Component {
   state = {
     series: []
   }
+
+  componentDidMount() {
+    const series = ["Game of Thrones", "One Punch Man", "Brooklyn Nine Nine"];
+    setTimeout(() => {
+      this.setState({ series });
+    }, 2000);
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">TV Series List</h1>
         </header>
-        <Intro message="Here you can find all of your most loved TV series"/>
+        <Intro message="Here you can find all of your most loved TV series" />
         the length of series array is: {this.state.series.length}
       </div>
     );
